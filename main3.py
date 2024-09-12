1895794b4dfa75f646e21cd2bd929354427dc4ae
bhukyaswapna14 <162866562+bhukyaswapna14@users.noreply.github.com>
2024-09-13 00:21:53 +0530

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,3 +1,7 @@
+#Topic: Custom Classes in Python
+
+
+#When iterated, it first returns the length in the correct format, followed by the width.
 class Rectangle:
     def _init_(self, length: int, width: int):
         self.length = length
